9a002289d7bffb21338e7a712afb9fda791df0bc
ExcelDsigN-tech <oluwadamilareoduyomi.com>
2026-05-16 18:12:29 +0100

    Add minimal Rust project for CI checks

---
+++ target/debug/deps/repoflow_indexer-1e46789abd84007a.d
@@ -0,0 +1,5 @@
+C:\Users\pc\drips\REPOFLOW\repoflow-indexer\target\debug\deps\repoflow_indexer-1e46789abd84007a.d: src\main.rs
+
+C:\Users\pc\drips\REPOFLOW\repoflow-indexer\target\debug\deps\repoflow_indexer-1e46789abd84007a.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/repoflow_indexer-638f97ee4028f7a8.d
@@ -0,0 +1,9 @@
+C:\Users\pc\drips\REPOFLOW\repoflow-indexer\target\debug\deps\repoflow_indexer-638f97ee4028f7a8.d: src\main.rs Cargo.toml
+
+C:\Users\pc\drips\REPOFLOW\repoflow-indexer\target\debug\deps\librepoflow_indexer-638f97ee4028f7a8.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
